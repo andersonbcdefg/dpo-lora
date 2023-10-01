eaa0e6d85139a741abea97cd1eb31ccf7c7fad08
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-09-30 20:04:31 -0700

    try again

--- dummy_ddp.py
+++ dummy_ddp.py
@@ -1,11 +1,16 @@
+import os
 import torch
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 if __name__ == "__main__":
+    local_rank = int(os.environ["LOCAL_RANK"]) 
+    world_size = int(os.environ["WORLD_SIZE"])
     torch.distributed.init_process_group(
         backend='nccl',
-        init_method='tcp://localhost:12345'
+        init_method='tcp://localhost:12345',
+        rank=local_rank,
+        world_size=world_size
     )
     rank = torch.distributed.get_rank()
     torch.cuda.set_device(f"cuda:{rank}")
