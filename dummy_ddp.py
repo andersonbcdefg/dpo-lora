6f65272dd49b0ce09186e752d12776619b0e3339
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-09-30 20:11:03 -0700

    new dummy

--- dummy_ddp.py
+++ dummy_ddp.py
@@ -1,20 +1,40 @@
-import os
 import torch
 import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-if __name__ == "__main__":
-    local_rank = int(os.environ["LOCAL_RANK"]) 
-    world_size = int(os.environ["WORLD_SIZE"])
-    torch.distributed.init_process_group(
-        backend='nccl',
-        init_method='gloo://',
-        rank=local_rank,
-        world_size=world_size
-    )
-    rank = torch.distributed.get_rank()
-    torch.cuda.set_device(f"cuda:{rank}")
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
 
-    # return world size
-    ws = torch.distributed.get_world_size()
-    print("World size:", ws)
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+
+def demo_basic():
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    print(f"Start running basic DDP example on rank {rank}.")
+
+    # create model and move it to GPU with id rank
+    device_id = rank % torch.cuda.device_count()
+    model = ToyModel().to(device_id)
+    ddp_model = DDP(model, device_ids=[device_id])
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(device_id)
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    demo_basic()
